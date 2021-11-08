f7c36c9cb1775d997094ef2320e356f4d036916d
Leon Langhoff <leon.langhoff@posteo.de>
2021-11-08 23:18:46 +0100

    finished result message system

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,18 +10,11 @@ class GamesController < ApplicationController
 
   def score
     starting_time =  Time.new
-    check = check_word(params[:score])
-    length = check_length(params[:score])
-    letters = check_letters(params[:score])
-    raise
-    if letters == false
-      @message = "Sorry but #{params["score"]} cant be build out of #{params["letters"].split(" ")}"
-    elsif check == false
-      @message = "Sorry, #{params["score"]} doesn't seem to be an english word"
-    else
-      @message = "Congratulations!"
-    end
-
+    @word = params[:score]
+    @letters = params["letters"].split(" ")
+    @check_word_exist = check_word(params[:score])
+    @length = check_length(params[:score])
+    @included = included?(@word, @letters)
   end
 
   private
@@ -40,10 +33,7 @@ class GamesController < ApplicationController
     html_doc["length"]
   end
 
-  def check_letters(word)
-    # url = "https://wagon-dictionary.herokuapp.com/#{word}"
-    # api_call = URI.open(url).read
-    # html_doc = JSON.parse(api_call)
-    params["letters"].split(" ").include?word.split("")
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 end
